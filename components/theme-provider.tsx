e46cabd632ff23da99351640b014bb497e389868
twfeeqg-thub <ebnsalemalawal@gmail.com>
2025-09-27 21:52:51 +0300

    Update theme-provider.tsx

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -2,8 +2,9 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
+import { type ThemeProviderProps } from "next-themes"
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+// هذا هو الكود الصحيح الذي يضيف خاصية children يدوياً
+export function ThemeProvider({ children, ...props }: ThemeProviderProps & { children: React.ReactNode }) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
